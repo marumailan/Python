ec587ae1ddebd69e84cb400476950854bcd13fd6
Maru Mailan <oscmailan@gmail.com>
2025-08-18 13:36:07 +0800

    updated

--- mileage_converter.py
+++ mileage_converter.py
@@ -1,5 +1,3 @@
-print("How many kilometers did you run today?")
-km = input()
+km = input("How many kilometers did you run today? \n")
 miles = round(float(km) / 1.60934,2)
-
 print(f"You have run {miles} miles")
